00eb8824faa0b796c20cfdaeb7a2c6a4c2814786
Shafique Jamal <shafique.jamal@gmail.com>
2018-03-09 16:16:43 -0500

    add parameters to method so that they don't have to be pulled from configuration

--- src/main/scala/com/github/shafiquejamal/accessapi/access/authentication/TokenValidator.scala
+++ src/main/scala/com/github/shafiquejamal/accessapi/access/authentication/TokenValidator.scala
@@ -1,11 +1,13 @@
 package com.github.shafiquejamal.accessapi.access.authentication
 
 
+import java.time.temporal.TemporalUnit
+
 import com.github.shafiquejamal.accessapi.user.UserDetails
 
 trait TokenValidator[US, T <: UserDetails[US]] {
   
-  def decodeAndValidateToken(token: String): Option[T]
+  def decodeAndValidateToken(token: String, jWTValidityLength: Long, jWTValidityUnit: TemporalUnit): Option[T]
   
 }
 
